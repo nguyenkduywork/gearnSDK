dcd28b0abf575e1fdb00c627cdc80f6d9b137969
nguyenkduy <nguyenkduywork@gmail.com>
2022-06-15 09:49:28 +0200

    Show Ethereum balance

--- Assets/Web3Unity/Scripts/Prefabs/EVM/EthBalanceOfExample.cs
+++ Assets/Web3Unity/Scripts/Prefabs/EVM/EthBalanceOfExample.cs
@@ -1,16 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EthBalanceOfExample : MonoBehaviour
 {
+    [SerializeField] private Text myText;
     async void Start()
     {
         string chain = "ethereum";
         string network = "rinkeby"; // mainnet ropsten kovan rinkeby goerli
-        string account = "0xdD4c825203f97984e7867F11eeCc813A036089D1";
+        string account = PlayerPrefs.GetString("Account");
 
         string balance = await EVM.BalanceOf(chain, network, account);
-        print(balance);
+        //change balance to double
+        double balanceDouble = double.Parse(balance);
+        //Divide by 10^18
+        balanceDouble = balanceDouble / 1000000000000000000;
+        //Convert to string
+        balance = balanceDouble.ToString();
+        myText.text = balance;
     }
 }
