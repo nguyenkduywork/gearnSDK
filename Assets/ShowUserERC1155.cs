d47a50967114d544e378afc633688c50e320b14f
nguyenkduy <nguyenkduywork@gmail.com>
2022-06-24 14:43:30 +0200

    Find all erc1155 contracts in a wallet

--- Assets/ShowUserERC1155.cs
+++ Assets/ShowUserERC1155.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.Networking;
+using Newtonsoft.Json;
 
 public class ShowUserERC1155 : MonoBehaviour
 {
     public class Response {
         public string image;
+    }
+    
+    private class NFTs
+    {
+        public string contract { get; set; }
+        public string tokenId { get; set; }
+        public string uri { get; set; }
+        public string balance { get; set; }
     }
 
     async void Start()
@@ -16,7 +25,25 @@ public class ShowUserERC1155 : MonoBehaviour
         string network = "rinkeby";
         string contract = "0xd1d06063Dc4281f0E6ebd6CBea1AAe9E20E471ea";
         string tokenId = "10";
-
+        string account = "0x5146DcCC1b386BC26BE120Ea41b1f4A27f796e89";
+        Debug.Log(account);
+        //Get all erc1155
+        
+        string response = await EVM.AllErc1155(chain, network, account, "", 500, 0);
+        try
+        {
+            NFTs[] erc1155s = JsonConvert.DeserializeObject<NFTs[]>(response);
+            //loop through all erc1155 and print out the contract
+            foreach (NFTs erc1155 in erc1155s)
+            {
+                Debug.Log(erc1155.contract);
+            }
+        }
+        catch
+        {
+            print("Error: " + response);
+        }
+        /*
         // fetch uri from chain
         string uri = await ERC1155.URI(chain, network, contract, tokenId);
         if (uri.StartsWith("ipfs://"))
@@ -44,5 +71,6 @@ public class ShowUserERC1155 : MonoBehaviour
         UnityWebRequest textureRequest = UnityWebRequestTexture.GetTexture(imageUri);
         await textureRequest.SendWebRequest();
         this.gameObject.GetComponent<Renderer>().material.mainTexture = ((DownloadHandlerTexture)textureRequest.downloadHandler).texture;
+        */
     }
 }
