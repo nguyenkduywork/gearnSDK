f42f522365c00f786006c4771499d82690aa0d98
nguyenkduy <nguyenkduywork@gmail.com>
2022-06-27 10:23:31 +0200

    Add a class to call solidity functions (preferably functions that requires to sign)

--- Assets/GearnSDK/MintButton/CallSolidityFuncForButton.cs
+++ Assets/GearnSDK/MintButton/CallSolidityFuncForButton.cs
@@ -3,56 +3,49 @@ using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class MintToken : MonoBehaviour
+public class CallSolidityFuncForButton : MonoBehaviour
 {
     //The button to mint a token
     public Button yourButton;
     
-    //GameManager object, used here to deduct in game coins
-    [SerializeField]
-    private GameManager gameManager;
-    
     //The user's wallet address
     string address;
-    //Function name in the smart contract
+    
+    //Function name in the smart contract, here is mint for example
     string method = "mint";
+    
     //Smart contract's abi
     string abi = "[ { \"inputs\": [], \"stateMutability\": \"nonpayable\", \"type\": \"constructor\" }, { \"anonymous\": false, \"inputs\": [ { \"indexed\": true, \"internalType\": \"address\", \"name\": \"owner\", \"type\": \"address\" }, { \"indexed\": true, \"internalType\": \"address\", \"name\": \"spender\", \"type\": \"address\" }, { \"indexed\": false, \"internalType\": \"uint256\", \"name\": \"value\", \"type\": \"uint256\" } ], \"name\": \"Approval\", \"type\": \"event\" }, { \"anonymous\": false, \"inputs\": [ { \"indexed\": true, \"internalType\": \"address\", \"name\": \"from\", \"type\": \"address\" }, { \"indexed\": true, \"internalType\": \"address\", \"name\": \"to\", \"type\": \"address\" }, { \"indexed\": false, \"internalType\": \"uint256\", \"name\": \"value\", \"type\": \"uint256\" } ], \"name\": \"Transfer\", \"type\": \"event\" }, { \"inputs\": [ { \"internalType\": \"address\", \"name\": \"owner\", \"type\": \"address\" }, { \"internalType\": \"address\", \"name\": \"spender\", \"type\": \"address\" } ], \"name\": \"allowance\", \"outputs\": [ { \"internalType\": \"uint256\", \"name\": \"\", \"type\": \"uint256\" } ], \"stateMutability\": \"view\", \"type\": \"function\" }, { \"inputs\": [ { \"internalType\": \"address\", \"name\": \"spender\", \"type\": \"address\" }, { \"internalType\": \"uint256\", \"name\": \"amount\", \"type\": \"uint256\" } ], \"name\": \"approve\", \"outputs\": [ { \"internalType\": \"bool\", \"name\": \"\", \"type\": \"bool\" } ], \"stateMutability\": \"nonpayable\", \"type\": \"function\" }, { \"inputs\": [ { \"internalType\": \"address\", \"name\": \"account\", \"type\": \"address\" } ], \"name\": \"balanceOf\", \"outputs\": [ { \"internalType\": \"uint256\", \"name\": \"\", \"type\": \"uint256\" } ], \"stateMutability\": \"view\", \"type\": \"function\" }, { \"inputs\": [ { \"internalType\": \"address\", \"name\": \"\", \"type\": \"address\" } ], \"name\": \"claimByAddress\", \"outputs\": [ { \"internalType\": \"uint256\", \"name\": \"\", \"type\": \"uint256\" } ], \"stateMutability\": \"view\", \"type\": \"function\" }, { \"inputs\": [], \"name\": \"decimals\", \"outputs\": [ { \"internalType\": \"uint8\", \"name\": \"\", \"type\": \"uint8\" } ], \"stateMutability\": \"view\", \"type\": \"function\" }, { \"inputs\": [ { \"internalType\": \"address\", \"name\": \"spender\", \"type\": \"address\" }, { \"internalType\": \"uint256\", \"name\": \"subtractedValue\", \"type\": \"uint256\" } ], \"name\": \"decreaseAllowance\", \"outputs\": [ { \"internalType\": \"bool\", \"name\": \"\", \"type\": \"bool\" } ], \"stateMutability\": \"nonpayable\", \"type\": \"function\" }, { \"inputs\": [ { \"internalType\": \"address\", \"name\": \"spender\", \"type\": \"address\" }, { \"internalType\": \"uint256\", \"name\": \"addedValue\", \"type\": \"uint256\" } ], \"name\": \"increaseAllowance\", \"outputs\": [ { \"internalType\": \"bool\", \"name\": \"\", \"type\": \"bool\" } ], \"stateMutability\": \"nonpayable\", \"type\": \"function\" }, { \"inputs\": [ { \"internalType\": \"address\", \"name\": \"to\", \"type\": \"address\" }, { \"internalType\": \"uint256\", \"name\": \"amount\", \"type\": \"uint256\" } ], \"name\": \"mint\", \"outputs\": [], \"stateMutability\": \"nonpayable\", \"type\": \"function\" }, { \"inputs\": [], \"name\": \"name\", \"outputs\": [ { \"internalType\": \"string\", \"name\": \"\", \"type\": \"string\" } ], \"stateMutability\": \"view\", \"type\": \"function\" }, { \"inputs\": [], \"name\": \"symbol\", \"outputs\": [ { \"internalType\": \"string\", \"name\": \"\", \"type\": \"string\" } ], \"stateMutability\": \"view\", \"type\": \"function\" }, { \"inputs\": [], \"name\": \"totalSupply\", \"outputs\": [ { \"internalType\": \"uint256\", \"name\": \"\", \"type\": \"uint256\" } ], \"stateMutability\": \"view\", \"type\": \"function\" }, { \"inputs\": [ { \"internalType\": \"address\", \"name\": \"to\", \"type\": \"address\" }, { \"internalType\": \"uint256\", \"name\": \"amount\", \"type\": \"uint256\" } ], \"name\": \"transfer\", \"outputs\": [ { \"internalType\": \"bool\", \"name\": \"\", \"type\": \"bool\" } ], \"stateMutability\": \"nonpayable\", \"type\": \"function\" }, { \"inputs\": [ { \"internalType\": \"address\", \"name\": \"from\", \"type\": \"address\" }, { \"internalType\": \"address\", \"name\": \"to\", \"type\": \"address\" }, { \"internalType\": \"uint256\", \"name\": \"amount\", \"type\": \"uint256\" } ], \"name\": \"transferFrom\", \"outputs\": [ { \"internalType\": \"bool\", \"name\": \"\", \"type\": \"bool\" } ], \"stateMutability\": \"nonpayable\", \"type\": \"function\" } ]";
+    
     //Contract's address
     string contract = "0xD40d1f9854e989225c88935E79d2EF0033d4369c";
+    
     //Value in wei to be sent to the contract, here 0
     string value = "0";
+    
     //Gas limit to be used for the transaction
     string gasLimit = "";
     string gasPrice = "";
+    
     //Arguments for the function call
     string args;
+    
     //Chain ID of the network the contract is deployed on, here Rinkeby so 4
     string chainId= "4";
+    
     //The data to be sent to the contract
     private string data;
-    //User's current in game cash
-    private double currentCash;
-
+    
     private string transaction;
     
     string chain = "ethereum";
     string network = "rinkeby";
-    private string txConfirmed;
-    private string errorMess;
-    async void Start()
+
+    private void Start()
     {
         //Initialize the button to mint tokens using
         Button btn = yourButton.GetComponent<Button>();
-        btn.onClick.AddListener(mintButton);
-    }
-    
-    //Retrieve all in-game money from the user (in the database)
-    public void retireMonnaie()
-    {
-        currentCash = Singleton<DataManager>.Instance.database.cash;
-        //Remove all in-game money from the user
-        gameManager.SetCash(-currentCash);
+        btn.onClick.AddListener(onClick);
     }
 
     //Create the transaction to the blockchain,
@@ -78,67 +71,28 @@ public class MintToken : MonoBehaviour
     async void getStatus()
     {
         try{
-            txConfirmed = await EVM.TxStatus(chain, network, transaction);
+            await EVM.TxStatus(chain, network, transaction);
             //reinitialize the transaction variable
             transaction = null;
         }
         catch(Exception e)
         {
-            txConfirmed = "error";
+            Debug.Log(e);
         }
-        
-        //Send a notification to the screen, this can be changed freely to fit the needs of the game
-        if(!txConfirmed.Equals("success"))
-        {
-            Notification.instance.Warning("Transaction failed or unrealized");
-            Singleton<SoundManager>.Instance.Play("Notification");
-        }
-        else
-        {
-            Notification.instance.Warning("Transaction successful");
-            Singleton<SoundManager>.Instance.Play("Notification");
-            
-            //if transaction is successful, take all of the in-game money
-            retireMonnaie();
-        }
-        //reenable the button
-        yourButton.enabled = true;
     }
     
     //Function called when the button is clicked,
-    //if the user has more than 185000 in game cash
-    private async void mintButton()
+    private async void onClick()
     {
-        //Disable the button to prevent multiple clicks
-        yourButton.enabled = false;
-        
-        //Get the current cash in the database, this depends on the game and how it is implemented
-        currentCash = Singleton<DataManager>.Instance.database.cash;
-        
-        if (currentCash <= 185000)
-        {
-            Notification.instance.Warning("Minimum coin required: 185000");
-            Singleton<SoundManager>.Instance.Play("Notification");
-            //Re-enable the button
-            yourButton.enabled = true;
-            return;
-        }
-        
         //Send the transaction to the blockchain
         try
         {
             await SendTransactionWeb3();
         }
-        catch
+        catch(Exception e)
         {
-            Notification.instance.Warning("Transaction canceled");
-            Singleton<SoundManager>.Instance.Play("Notification");
-            getStatus();
-            return;
+            Debug.Log(e);
         }
-
-        Notification.instance.Warning("Processing transaction");
-        Singleton<SoundManager>.Instance.Play("Notification");
         //Wait for 25 seconds for the transaction to be confirmed on the blockchain
         Invoke("getStatus",25);
     }
